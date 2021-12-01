8b7f3a891f70fdcc6ba1d79660444971a8aa61e1
golf <645059550@qq.com>
2021-12-01 14:16:30 +0800

    demo

--- src/views/demo/index.jsx
+++ src/views/demo/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Card } from "antd";
 import TypingCard from '@/components/TypingCard'
 const Demo = () => {
   const cardContent = '这是演示页面 5 4 3 2 1'
   return (
     <div className="app-container">
-      <TypingCard title='演示页面' source={cardContent}/>
+      <TypingCard title='演示页面' source={cardContent} />
+      <div>分割线</div>
+      <Card bordered={false} className="card-item" title='演示页面'>
+        <div>{cardContent}</div>
+      </Card>
     </div>
   );
 }
